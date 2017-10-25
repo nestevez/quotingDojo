07ac61f1530895a2198fbf571a57fb8f8d57ec5d
Natalia <nataliae.dev@gmail.com>
2017-10-24 19:05:16 -0700

    Initial commit, home page

---
+++ quotingDojo/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using DbConnection;
+
+namespace quotingDojo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /Home/
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("/quotes/")]
+        public IActionResult Quotes()
+        {
+            List<Dictionary<string, object>> AllQuotes = DbConnector.Query("SELECT * FROM quotes");
+            ViewBag.allquotes = AllQuotes;
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/quotes/")]
+        public IActionResult PostQuotes(string name, string quote)
+        {
+            return RedirectToAction("Quotes");
+        }
+    }
+}
